37bdb12cd8c7cd950818fe1130b6d785599efb75
Ben Lavallée <blavallee@lcs.on.ca>
2025-04-03 11:22:18 -0400

    Initial Commit

---
+++ AP Create Task/AP_Create_TaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  AP_Create_TaskApp.swift
+//  AP Create Task
+//
+//  Created by Benjamin Lavallee on 2025-04-03.
+//
+
+import SwiftUI
+
+@main
+struct AP_Create_TaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AP Create TaskTests/AP_Create_TaskTests.swift
@@ -0,0 +1,17 @@
+//
+//  AP_Create_TaskTests.swift
+//  AP Create TaskTests
+//
+//  Created by Benjamin Lavallee on 2025-04-03.
+//
+
+import Testing
+@testable import AP_Create_Task
+
+struct AP_Create_TaskTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
